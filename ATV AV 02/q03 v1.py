714430de50135ff0a5fcf44af959871120bbfade
Soca-Fofo-py <rawennellen@gmail.com>
2025-06-11 16:32:55 -0300

    Add files via upload

---
+++ ATV AV 02/q03 v1.py
@@ -0,0 +1,11 @@
+n = int(input("Digite um número: "))
+m = int(input("Digite um segundo número: "))
+fat = 1 #Valor innicial neutro de fat
+
+############################################
+for x in range(1,n+1):
+    fat = fat*x
+############################################
+for m in range(n+1,m):
+    if fat % m == 0:
+        print(m,"é um Jaime de",n)
