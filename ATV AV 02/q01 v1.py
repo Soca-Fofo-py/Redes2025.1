82911b4d3d93829408d997a947a3c89c21d0704c
Soca Fofo <rawennellen@gmail.com>
2025-06-10 22:15:41 -0300

    Update de v1

--- ATV AV 02/q01 v1.py
+++ ATV AV 02/q01 v1.py
@@ -1,12 +1,7 @@
-num_1 = input("Digite um número: ")
-num_2 = 0
+num_1 = input("Digite um número: ") # temos uma string
+num_2 = 0 # Um numero vazio
 
-int_1  = int(num_1)
-
-for x in num_1:
-    num_2 = int(x)*10 + num_2
-    print(num_2)
-print(num_2)
-
-
-#print(num_1,int_1)
+for x in num_1[::-1]:
+    num_2 = int(x) + num_2*10
+if int(num_1) == num_2:
+    print(num_2,"é um palíndromo!")
