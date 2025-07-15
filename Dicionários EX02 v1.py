cde1a4e958ef44fce1eb1be46501edebd4479eb4
Soca Fofo <rawennellen@gmail.com>
2025-07-15 08:02:07 -0300

    2025.07.15

--- Dicionários EX02 v1.py
+++ Dicionários EX02 v1.py
@@ -1,6 +1,5 @@
-import requests, json
-
-clubes = requests.get("https://api.cartolafc.globo.com/clubes").txt
+import requests, json # pip install requests
+clubes = requests.get("https://api.cartolafc.globo.com/clubes").text
 dClubes = json.loads(clubes)
 for clube in dClubes.values():
     if clube["nome"] == "BOT":
