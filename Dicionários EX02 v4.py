82b0f4ee516899e7d52ca3e680786e4e8e051e55
Soca Fofo <rawennellen@gmail.com>
2025-07-16 10:35:23 -0300

    5 mais caros

--- Dicionários EX02 v4.py
+++ Dicionários EX02 v4.py
@@ -5,16 +5,21 @@ try:
     dados = json.loads(dados)
     atletas = dados["atletas"]
     posicoes = dados["posicoes"]
+    #print(posicoes.values())
     botafogo = []
     clubes = dados["clubes"]
     clube_id = 0
-    var = input("Qual o nome do time: ").upper()
+    var = "FLAMENGO"#input("Qual o nome do time: ").upper()
     for clube in clubes.values():
         if var == clube["nome_fantasia"].upper():
             clube_id = clube["id"]
-            print(f"Time {var}, --> {clube_id}")
+            print(f"Time: {var}, com ID: {clube_id}")
             break
-    
+    poslist = []
+    for x in posicoes.values():
+        #print((x["nome"]))
+        poslist.append(x["nome"])
+    #print(poslist)
 
     if clube_id !=0:
         for atleta in atletas:
@@ -22,9 +27,21 @@ try:
                 nome_posicao = posicoes[str(atleta['posicao_id'])]["nome"]
                 botafogo.append([ atleta['nome'] , atleta['apelido'] , nome_posicao , atleta['preco_num'] ])
         botafogo.sort (key=lambda x: x[3],reverse=True)
-        print(botafogo)
+        #print(botafogo)
+        for atleta in botafogo:
+            #print(atleta[2])
+            if atleta[2] in poslist:
+                print(f"{atleta[2]:10s} -> {atleta[1]:20s} -> {atleta[0]:50s} -> {atleta[3]:5.2f} ")
+                bar = poslist.index(atleta[2])
+                #print(bar)
+                if bar > -1:
+                    poslist.pop(bar)
+        #print(poslist)
+
+        '''
         for atleta in botafogo:
             print(f"{atleta[2]:10s} -> {atleta[1]:20s} -> {atleta[0]:50s} -> {atleta[3]:5.2f} ")
+        '''
     
 except json.decoder.JSONDecodeError as e:
     print ("Erro na conversão de JSON para dicionario")
