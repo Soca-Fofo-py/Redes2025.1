fea80384caaecb13d0b3fb9d5d6960bb2bd5b946
Soca Fofo <rawennellen@gmail.com>
2025-08-06 07:54:15 -0300

    07:54 da manhã

--- ATV AV 03/q1 v1.py
+++ ATV AV 03/q1 v1.py
@@ -15,10 +15,8 @@ import json
 import cpf # Importa uma função para validação de CPF!
 
 
-tabela_cpf_mac = {12345678900:['00:1A:2B:3C:4D:5E'] , 78945612300:['00:1C:2A:3B:4D:5E'] , 
-                   "11630408433":[]} #
-#tabela_cpf_mac['11630408433'] = 145
-#print(tabela_cpf_mac)
+tabela_cpf_mac = {12345678900:['00:1A:2B:3C:4D:5E','00:1C:2A:3B:4D:5E'] ,'11630408433':['00:1A:2B:3C:4D:5E']} #
+#print(tabela_cpf_mac) # testes
 
 ###################################################################################################################
 # Função do menu principal!
@@ -55,20 +53,18 @@ def check_mac(mac : str) -> bool:
     print("MAC Válido")
     return True
 ###################################################################################################################
-
-
-
-
-
+###################################################################################################################
 
 # Chamado da função menu!
 opcao = menu_principal()
 ###################################################################################################################
+###################################################################################################################
 # Cadastra um CPF!
-if opcao == 1:
-    print("Cadastrar um CPF\n")
+if opcao == 1: # a
+    print("Cadastrar um CPF!\n")
     var_1= input("Digite o CPF a qual deseja cadastrar: ")
     if cpf.cpf_valido(var_1) == True:
+        #
         # This method adds a key with a specified default value if the key is not already present in the dictionary.
         tabela_cpf_mac.setdefault(var_1,[])
     else:
@@ -76,113 +72,112 @@ if opcao == 1:
 ###################################################################################################################
 ###################################################################################################################
 # Adicionar MAC address a um CPF!
-if opcao == 2: 
-    print("Adicionar MAC address a um CPF\n")
+if opcao == 2:  # b
+    print("Adicionar MAC address a um CPF!\n")
     var_3 = input("Digite a qual CPF deseja cadastrar um endereço MAC: ")
     if cpf.cpf_valido(var_3) == True:
-        if (str(var_3)) in tabela_cpf_mac:
+        if (var_3) in tabela_cpf_mac.keys():
             var_4 = input("Digite o endereço MAC que deseja cadastrar: ")
             if check_mac(var_4) == True:
-                lista_temp = []
-                lista_temp = tabela_cpf_mac[var_3]
-                #print("temp",lista_temp,type(var_3),type(lista_temp)) #############
+                lista_temp = [] # lista vazia
+                lista_temp = tabela_cpf_mac[var_3] # lista recebe valor da chave cpf
+                #print("temp",lista_temp,type(var_3),type(lista_temp)) ############# # testes
                 lista_temp = lista_temp.append(var_4)
-                print(tabela_cpf_mac[var_3],"455") ##############
+                #print(tabela_cpf_mac[var_3],"455") ############## # testes
                 tabela_cpf_mac[var_3] = lista_temp #???????????????????????
-                print("temp",tabela_cpf_mac[var_3],456)
-                print(var_3,"\n",
-                      tabela_cpf_mac,"\n",
-                      var_4,
-                      "test") ####
+                #print("temp",tabela_cpf_mac[var_3],456) # testes
+            else:
+                print("O endereço MAC que deseja cadastrar é inválido!")
+        else:
+            print("O CPF informado não está cadastrado!")
+    else:
+        print("CPF Inválido!")       
+###################################################################################################################
+###################################################################################################################
+if opcao == 3:  # c
+    print("Remover um MAC address de um CPF!\n")
+    var_5 = input("Digite a qual CPF deseja REMOVER um endereço MAC: ")
+    if cpf.cpf_valido(var_5) == True:
+        if (var_5) in tabela_cpf_mac.keys():
+            var_6 = input("Digite o endereço MAC que deseja REMOVER: ")
+            if check_mac(var_6) == True:
+                lista_temp2 = [] # lista vazia
+                lista_temp2 = tabela_cpf_mac[var_5] # lista recebe valor da chave cpf
+                if var_6 in lista_temp2:
+                    lista_temp2 = lista_temp2.pop(lista_temp2.index(var_6))
+                    tabela_cpf_mac[var_5] = lista_temp2
+                    #rint("temp",tabela_cpf_mac[var_5],777) # testes
+                else:
+                    print("O endereço MAC que deseja REMOVER não está vinculado a este CPF!")
             else:
                 print("O endereço MAC que deseja cadastrar é inválido!")
         else:
             print("O CPF informado não está cadastrado!")
     else:
         print("CPF Inválido!")
-    #print(var_3,lista_temp,var_4,"test2")####
-
 ###################################################################################################################
+###################################################################################################################
+if opcao == 4:  # d
+    print("Remover um CPF cadastrado!\n")
+    var_7 = input("Digite o CPF que deseja REMOVER: ")
+    if cpf.cpf_valido(var_7) == True:
+        if (var_7) in tabela_cpf_mac.keys():
+            lista_temp3 = [] # lista vazia
+            lista_temp3 = tabela_cpf_mac[var_7] # lista recebe valor da chave cpf
+            if len(lista_temp3) == 0:
+                del tabela_cpf_mac[var_7]
+            else:
+                print("O CPF que deseja remover contem MACs a ele associados e por isso não pode ser removido!")
+        else:
+            print("O CPF informado não está cadastrado!")
+    else:
+        print("CPF Inválido!")
+###################################################################################################################
+###################################################################################################################
+# Lista os CPF cadastrados!
+if opcao == 5: # e
+    print("listando os CPF cadastrados!")
+    for var in tabela_cpf_mac.keys():
+        print("CPF:",var)    
+###################################################################################################################
+###################################################################################################################
+# Lista os MAC adresses vinculados a um CPF!
+if opcao == 6: # f
+    print("Listar os MAC adresses vinculados a um CPF!\n")
+    var_8 = input("Digite o CPF ao qual deseja cheacar os MACs a ele vinculado: ")
+    if cpf.cpf_valido(var_8) == True:
+        if (var_8) in tabela_cpf_mac.keys():
+            lista_temp4 = [] # lista vazia
+            lista_temp4 = tabela_cpf_mac[var_8] # lista recebe valor da chave cpf
+            if len(lista_temp4) > 0:
+                print(f"Listando os MAC adresses vinculados ao CPF: {var_8}")
+                for y in lista_temp4:
+                    print (y)
+            else:
+                print("O CPF informado não contem MACs a ele associado!")
+        else:
+            print("O CPF informado não está cadastrado!")
+    else:
+        print("CPF Inválido!")
+###################################################################################################################
+###################################################################################################################
+
+# print(tabela_cpf_mac) # testes
+
+
+
+################################################
 '''
-elif opcao == 21:
-    print("remover mac")
-    #amigos_ate(10000)
-elif opcao == 21:
-    print("remover mac")
-    #amigos_ate(10000)
-else:
-    print ("Opcao invalida.")
-'''
-print(tabela_cpf_mac)
-
-
-
-
-
-
-'''
-comando = 9 # De 1 a 0, o 9 estava sobrando!
 tabela_cpf_mac = {12345678900:['00:1A:2B:3C:4D:5E','00:1B:2B:3A:4D:5C'] , 78945612300:['00:1C:2A:3B:4D:5E'] , 
                   74185296300:[]}
 
-while comando != 0:
-    comando = int(input("\n" + f"{'CÓDIGO':^6s} : AÇÃO QUE DESEJA REALIZAR\n" + 
-                    f"{'1':^6s} : Cadastrar CPF\n" + f"{'2':^6s} : Adicionar MAC address a um CPF\n" + 
-                    f"{'3':^6s} : Remover um MAC address de um CPF\n" + f"{'4':^6s} : Remover o CPF\n" + 
-                    f"{'5':^6s} : listar os CPF cadastrados\n" + 
-                    f"{'6':^6s} : listar os MAC adresses vinculados a um CPF\n" + 
-                    f"{'7':^6s} : Salvar o “banco de dados” em um arquivo\n" + 
-                    f"{'8':^6s} : ler o “banco de dados” de um arquivo\n" + 
-                    f"{'0':^6s} : Finaliza o programa\n\n" + "Digite o código da ação que deseja realizar: "
-                    ))
-    
-    # Lista os CPF cadastrados!
-    if comando == 5: # 5
-        print("listando os CPF cadastrados!")
-        for var in tabela_cpf_mac.keys():
-            print("CPF:",var)
-
-    # Lista os MAC adresses vinculados a um CPF!
-    if comando == 6: # 6
-        try:
-            # Uso 11 dígitos e int() como verificação de CPF!
-            CPF = int(input("Digite o CPF ao qual deseja cheacar os MACs a ele vinculado: "))
-            if len(str(CPF)) == 11: 
-                if CPF in tabela_cpf_mac.keys():
-                    print(f"Listando os MAC adresses vinculados ao CPF: {CPF}")
-                    print(tabela_cpf_mac.get(CPF,"Não há MACs vinculados a esse CPF"))
-                else: print(f"Esse CPF '{CPF}' não está cadastrado!")
-            else: print(f"O CPF: {CPF} é invalido!") # Diferente de 11
-        except: print(f"O CPF informado é inválido!") # Diferente de inteiros
-
-
-print("Código encerrado!")
-'''
-################################################
-'''
 import json
 d = json.loads(texto) # Carrega banco de dados
 t = json.dumps(d) #Converte dicionário em texto
-'''
-################################################
 
-
-
-
-
-'''
-tabela2 = {123654:[],987456:[]}
-tabela = {2:"ok",45:55,"cpf":""}
-print((tabela.items()))
-tabela["cpf"]=(123456)
-print((tabela.items()))
-print((tabela2.items())) 
-'''
-
-
-
-'''
 try:
 except:
     print("Em algum lugar, de alguma forma, algo deu errado e a culpa provavelmente foi sua!")
+
 '''
+################################################
